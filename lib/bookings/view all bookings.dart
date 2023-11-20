9f89c4e5ef4a7e29fc65f4c4f6f35afc28115c59
Jayfablead <121211943+Jayfablead@users.noreply.github.com>
2023-11-20 09:38:18 +0530

    20/11

--- lib/bookings/view all bookings.dart
+++ lib/bookings/view all bookings.dart
@@ -286,7 +286,7 @@ class _BookingListState extends State<BookingList> {
                                                                       index]
                                                                   .supplierDetails
                                                                   ?.address ??
-                                                              "",
+                                                               "",
                                                       maxLines: 2,
                                                       style: TextStyle(
                                                           color: Colors.black
